cb36721e96cce910cb1b234f68fce0ea16546f87
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-06-27 22:21:27 +0600

    about page remove

--- app/(WithCommonLayout)/about/page.tsx
+++ app/(WithCommonLayout)/about/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const page = () => {
+
+
     return (
         <div>
-            This is about page 
+            {/* This is about page  */}
             
         </div>
     );
