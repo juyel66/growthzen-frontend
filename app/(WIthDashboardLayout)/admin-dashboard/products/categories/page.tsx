f701fa89435c435bb7187506ca1614b1555689c9
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-08-03 04:15:01 +0600

    feat: create AdminProductsCategoriesPage component to render AdminCategoriesPage

---
+++ app/(WIthDashboardLayout)/admin-dashboard/products/categories/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import React from 'react';
+import AdminCategoriesPage from '@/app/(WIthDashboardLayout)/admin-dashboard/categories/page';
+
+export default function AdminProductsCategoriesPage() {
+  return <AdminCategoriesPage />;
+}
