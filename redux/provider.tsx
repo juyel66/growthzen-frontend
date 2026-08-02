3adb61b9e3bcb1bb39acf18c4fa5ac71d3b402a1
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-08-03 02:56:54 +0600

    feat: wrap children in AuthInitializer within ReduxProvider

--- redux/provider.tsx
+++ redux/provider.tsx
@@ -3,6 +3,7 @@
 import React, { useRef } from 'react';
 import { Provider } from 'react-redux';
 import { makeStore, AppStore } from './store';
+import AuthInitializer from '@/components/auth/AuthInitializer';
 
 interface ProviderProps {
   children: React.ReactNode;
@@ -10,12 +11,17 @@ interface ProviderProps {
 
 export default function ReduxProvider({ children }: ProviderProps) {
   const storeRef = useRef<AppStore | null>(null);
-  
+
   if (!storeRef.current) {
     // Create the store instance the first time this renders
     storeRef.current = makeStore();
   }
 
-  return <Provider store={storeRef.current}>{children}</Provider>;
+  return (
+    <Provider store={storeRef.current}>
+      <AuthInitializer>{children}</AuthInitializer>
+    </Provider>
+  );
 }
+
 export { ReduxProvider };
